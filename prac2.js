42416aa62be64ca30f1e1f7cc5d31a24300cd3e4
vivekshelke712 <vivekshelke006@gmail.com>
2024-10-16 10:36:55 +0530

    First Commit

---
+++ prac2.js
@@ -0,0 +1,2 @@
+// Real-time Seating Arrangement:
+// Create a 2-D array to represent seating arrangements for a movie theater. The system should handle seat booking and cancellation requests dynamically. Add constraints for group bookings, ensuring they are seated together.
